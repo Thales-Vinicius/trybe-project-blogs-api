433df717e714719c2d028c9538cf9e686c1b63b3
Thales <thalessilva.tv@gmail.com>
2022-03-15 16:12:48 -0300

    auth middleware validation

---
+++ src/middlewares/authValidation.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.header.authorization;
+  
+    if (!token) return res.status(401).json({ message: 'Token not found' });
+    
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.tokenData = decoded.data;
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
