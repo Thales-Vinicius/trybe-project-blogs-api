db9ca6e16a464c7f08137b4ccaa7f0517e3e67b2
Thales <thalessilva.tv@gmail.com>
2022-03-15 12:02:11 -0300

    models + associates

---
+++ src/models/BlogPost.js
@@ -0,0 +1,51 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Users',
+      key: 'id',
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  },
+  published: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  updated: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+};
+
+module.exports = (sequelize) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    Attributes,
+    {
+      timestamps: false,
+      tableName: 'BlogPosts',
+    },
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreingKey: 'userId', as: 'user' });
+  };
+};
