a23436bf9a7f3689daf160cd753456153bf69ad8
Thales <thalessilva.tv@gmail.com>
2022-03-15 17:56:05 -0300

    name, password, email middlewares validation

---
+++ src/middlewares/emailValidate.js
@@ -0,0 +1,11 @@
+module.exports = (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) return res.status(400).json({ message: '"email" is required' });
+
+  if (!email.match(/\S+@\S+\.\S+/)) {
+    return res.status(400).json({ message: '"email" must be a valid email' });
+  }
+
+  next();
+};
---
+++ src/middlewares/index.js
@@ -0,0 +1,11 @@
+const authValidate = require('./authValidate');
+const nameValidate = require('./nameValidate');
+const emailValidate = require('./emailValidate');
+const passwordValidate = require('./passwordValidate');
+
+module.exports = {
+   authValidate,
+   nameValidate,
+   emailValidate,
+   passwordValidate,
+};
