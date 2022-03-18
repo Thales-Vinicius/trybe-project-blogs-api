36a2cdf1d1bb84b6fdd2f4f0ea559bed53c183f8
Thales <thalessilva.tv@gmail.com>
2022-03-18 15:11:45 -0300

    update validations for post login

--- src/middlewares/emailValidate.js
+++ src/middlewares/emailValidate.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
   const { email } = req.body;
+  if (email === '') return res.status(400).json({ message: '"email" is not allowed to be empty' });
 
   if (!email) return res.status(400).json({ message: '"email" is required' });
 
--- src/middlewares/passwordValidate.js
+++ src/middlewares/passwordValidate.js
@@ -1,5 +1,8 @@
 module.exports = (req, res, next) => {
   const { password } = req.body;
+  if (password === '') { 
+    return res.status(400).json({ message: '"password" is not allowed to be empty' });
+  }
 
   if (!password) return res.status(400).json({ message: '"password" is required' });
 
